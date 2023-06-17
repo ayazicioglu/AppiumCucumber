1ead97d10a301ca531f6bc2f68dc51d0ea9b0309
AHMET YAZICIOGLU <aahmetyazicioglu@gmail.com>
2023-06-17 17:16:37 +0300

    17.6.23

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty",
+                "json:target/cucumber.json",},
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@us02",
+        dryRun = false
+)
+public class TestRunner {
+}
