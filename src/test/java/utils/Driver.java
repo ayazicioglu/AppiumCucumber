1828eb5fd47a38305732b5a06cff88cfc73d3987
AHMET YAZICIOGLU <aahmetyazicioglu@gmail.com>
2023-06-22 20:14:32 +0300

    22.6.23 ayazicioglu

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -11,12 +11,12 @@ import java.util.concurrent.TimeUnit;
 public class Driver {
     private static AndroidDriver appiumDriver;
 
-    static final String TELEFONADI="Pixel_3a_API_33_x86_64";
-    static final String ANDROIDVERSION="13.0";
-    static final String PLATFORM="Android";
-    static final String OTOMASYON_ISMI="UiAutomator2";
+    static final String TELEFONADI = "Pixel_3a_API_33_x86_64";
+    static final String ANDROIDVERSION = "13.0";
+    static final String PLATFORM = "Android";
+    static final String OTOMASYON_ISMI = "UiAutomator2";
 
-    public static AndroidDriver getAndroidDriver()  {
+    public static AndroidDriver getAndroidDriver() {
         URL appiumServerURL = null;
         try {
             appiumServerURL = new URL("http:127.0.0.1:4723/wd/hub");
@@ -26,22 +26,22 @@ public class Driver {
 
         if (appiumDriver == null) {
 
-            DesiredCapabilities caps =new DesiredCapabilities();
+            DesiredCapabilities caps = new DesiredCapabilities();
             caps.setCapability(MobileCapabilityType.DEVICE_NAME, TELEFONADI);
             caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, ANDROIDVERSION);
             caps.setCapability(MobileCapabilityType.PLATFORM_NAME, PLATFORM);
             caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, OTOMASYON_ISMI);
-            caps.setCapability("appPackage","com.ailebutcem");
-            caps.setCapability("appActivity","com.ailebutcem.MainActivity");
-            caps.setCapability(MobileCapabilityType.NO_RESET,false); // true uygulama sifirlanmiyor onceki adimlari muhafaza ediyor
+            caps.setCapability("appPackage", "com.ailebutcem");
+            caps.setCapability("appActivity", "com.ailebutcem.MainActivity");
+            caps.setCapability(MobileCapabilityType.NO_RESET, false); // true uygulama sifirlanmiyor onceki adimlari muhafaza ediyor
             //false ise her test baslangicinda sifirliyorss
 
             if (ConfigReader.getProperty("platformName").equals("Android")) {
 
                 assert appiumServerURL != null;
-                appiumDriver = new AndroidDriver<>(appiumServerURL,caps);
+                appiumDriver = new AndroidDriver<>(appiumServerURL, caps);
                 appiumDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-            }else {
+            } else {
 
                 throw new UnsupportedOperationException("Invalid Platform Name ");
 
@@ -53,7 +53,7 @@ public class Driver {
     }
 
 
-    public static void quitAppiumDriver(){
+    public static void quitAppiumDriver() {
         if (appiumDriver != null) {
             appiumDriver.closeApp();
             appiumDriver = null;
